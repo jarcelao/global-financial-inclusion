97d18565a7cc8afec4124cfd718416e95fb4dce6
Jericho Arcelao <jerichoarcelao@gmail.com>
2024-11-26 14:28:47 +0000

    feat: streamlit welcome page

---
+++ dashboard_streamlit/Welcome.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome | Global Findex 2021 Dataset",
+    page_icon="👋"
+)
+
+st.markdown(
+    """
+    # Welcome to the Global Findex Dataset Dashboard 👋
+
+    Use the pages in the sidebar to navigate the webpage.
+    - The 📊 Overview page shows a high-level summary of the dataset.
+    - The 🌏 Regional Insights page highlights regional trends and disparities.
+    - The 👥 Demographics page highlights demographic disparities.
+    - The 🚧 Barriers to Inclusion page looks into reasons for lack of access to financial services.
+    - The 📱 Digital Transformation tab shows digital financial service adoption.
+    """
+)
