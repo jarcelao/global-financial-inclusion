6488740ffbfea3476a9b191725e0ee8b648c315b
Jericho Arcelao <jerichoarcelao@gmail.com>
2024-12-01 17:16:05 +0000

    feat: remove dim_question

--- pipeline_dbt/models/marts/fct_response.sql
+++ pipeline_dbt/models/marts/fct_response.sql
@@ -1,11 +1,8 @@
 select
     s.wpid_random as respondent_id,
     s.economycode as economy_id,
-    q.question_id,
+    s.question,
     s.response,
     s.year
 from
     {{ ref('int_surveys_pivoted_to_responses') }} as s
-inner join
-    {{ ref('dim_question') }} as q
-    on s.question = q.question
